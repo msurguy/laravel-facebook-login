a3d23073e83178003a5531ef8cea087b33ff29a4
Maks Surguy <msurguy@gmail.com>
2015-03-14 20:56:21 -0700

    added local database file

---
+++ app/config/local/database.php
@@ -0,0 +1,20 @@
+<?php
+
+return array(
+
+    'connections' => array(
+
+        'mysql' => array(
+            'driver'    => 'mysql',
+            'host'      => 'localhost',
+            'database'  => 'facebook-login',
+            'username'  => 'root',
+            'password'  => 'root',
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+        ),
+
+    ),
+
+);
